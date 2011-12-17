172734eb6ac781bcb5ace6cce4f414c33e68b09f
kk_Ataka <kk_ataka@ring.skr.jp>
2011-12-17 14:33:50 +0900

    Load Errorが起きていたのを修正、テスト用ノートブックを変更。

--- spec/myEvernote_spec.rb
+++ spec/myEvernote_spec.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
 require 'rspec'
-require 'MyEvernote'
+require 'myEvernote'
 require 'kconv'
 require 'pp'
 
@@ -14,30 +14,32 @@ end
 
   before do
     @e = MyEvernote.new()
-    @SandboxGuid = '33880e53-4c9f-4104-a6e6-777ed1e3cef2'
-    @SandboxName = 'Sandbox'
+    @UpDelNotebookGuid = '71cdd6f9-5070-4508-bc80-a3f835a61a55'
+    @UpDelNotebookName = 'UpDeleteNotebook'
+    @NotebookGuid = '450b52e6-2daa-4b04-9012-4623a8e12ef5'
+    @NotebookName = 'TestNotebook'
   end
   describe 'ノートブックを取得するとき' do
     it '正常にログインできる' do
       @e.authentication.user.username.should be == "kk_ataka_t"
     end
     it '正常にGUIDとノートブック名が対応づけられている' do
-      @e.notebooks[@SandboxGuid].should be == @SandboxName
-      @e.notebooks.index(@SandboxName).should be == @SandboxGuid
+      @e.notebooks[@NotebookGuid].should be == @NotebookName
+      @e.notebooks.index(@NotebookName).should be == @NotebookGuid
     end
     it '全ノートブックを取得できる' do
-      @e.getNotebooks().length.should be 2
+      @e.getNotebooks().length.should be 4
     end
     it '特定のノートブックを取得できる(GUID)' do
-      @e.getNotebook(@SandboxGuid).name.should be == @SandboxName
+      @e.getNotebook(@NotebookGuid).name.should be == @NotebookName
     end
     it 'ノートブック内のノートを取得できる' do
-      pp @e.getNote(@SandboxGuid)
+      pp @e.getNote(@NotebookGuid)
     end
   end
   describe 'GUIDの妥当性を確認するとき' do
     it 'GUIDは妥当性である' do
-      @e.isGuid(@SandboxGuid).should be true
+      @e.isGuid(@NotebookGuid).should be true
     end
     it '妥当なGUIDではない(妥当ではない文字)' do
       @e.isGuid('XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX').should be false
